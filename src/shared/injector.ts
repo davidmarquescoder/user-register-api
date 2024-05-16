0b2175f01bc932af1b89ce5396d98e0b2fd32b6f
David Marques <davidmarques99@hotmail.com>
2024-05-16 15:42:51 -0300

    feat: usecase dependency injection implemented within controllers

--- src/shared/injector.ts
+++ src/shared/injector.ts
@@ -1,9 +1,11 @@
 // Imports
 import Controllers from '@/modules/users/interface-adapters/controllers/controllers';
+import UseCases from '@/modules/users/application/use-cases/useCases';
 
 export default class Injector {
   static createController(): Controllers {
-    const controllers = new Controllers();
+    const useCase = new UseCases();
+    const controllers = new Controllers(useCase);
 
     return controllers;
   }
