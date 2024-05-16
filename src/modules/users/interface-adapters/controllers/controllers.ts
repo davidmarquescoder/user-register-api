b18c303156e32be3547d490310f86cb446c07f7f
David Marques <davidmarques99@hotmail.com>
2024-05-16 15:40:37 -0300

    feat: implemnets create and index logic to controllers

--- src/modules/users/interface-adapters/controllers/controllers.ts
+++ src/modules/users/interface-adapters/controllers/controllers.ts
@@ -3,27 +3,37 @@ import { Request, Response } from 'express';
 
 // Contracts Imports
 import { ControllersInterface } from '../../domain/contracts/controllersInterface';
+import { UsecasesInterface } from '../../domain/contracts/useCasesInterface';
 
 export default class Controllers implements ControllersInterface {
-  constructor() {}
+  constructor(private readonly useCases: UsecasesInterface) {}
 
-  async index(request: Request, response: Response) {
+  async index(request: Request, response: Response): Promise<Response> {
+    const users = await this.useCases.index();
+    return response.status(200).json(users);
+  }
+
+  async show(request: Request, response: Response): Promise<Response> {
     return response.status(200).json({});
   }
 
-  async show(request: Request, response: Response) {
+  async create(request: Request, response: Response): Promise<Response> {
+    const data = (({ name, email, password, phone }) => ({
+      name,
+      email,
+      password,
+      phone,
+    }))(request.body);
+
+    const user = await this.useCases.create(data);
+    return response.status(201).json({ user });
+  }
+
+  async update(request: Request, response: Response): Promise<Response> {
     return response.status(200).json({});
   }
 
-  async create(request: Request, response: Response) {
-    return response.status(201).json({});
-  }
-
-  async update(request: Request, response: Response) {
-    return response.status(200).json({});
-  }
-
-  async delete(request: Request, response: Response) {
+  async delete(request: Request, response: Response): Promise<Response> {
     return response.status(200).json({});
   }
 }
